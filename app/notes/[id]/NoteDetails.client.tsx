bbc2e972a23ac3021a1586b2de7e45bb005fdabc
Tanya <yakovetsta1@gmail.com>
2026-03-29 21:10:22 -0300

    updates NoteDetails.client.tsx

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -13,6 +13,7 @@ export default function NoteDetailsClient() {
     queryKey: ["note", id],
     queryFn: () => fetchNoteById(id),
     enabled: Boolean(id),
+    refetchOnMount: false,
   });
 
   if (isLoading) {
